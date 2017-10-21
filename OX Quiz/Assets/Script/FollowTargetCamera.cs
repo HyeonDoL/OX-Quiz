f7d200edc316f9c6a60245036bc7c717f738cb4a
DESKTOP-B3EKVPO\YEOBI <wts1597@gmail.com>
2017-10-22 00:17:48 +0900

    Ingame scene desing, UI

---
+++ OX Quiz/Assets/Script/FollowTargetCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetCamera : MonoBehaviour {
+
+    public Transform m_target;
+
+    private Vector3 m_currentPos;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        m_currentPos = Vector3.Lerp(m_currentPos, m_target.position * 0.1f, Time.deltaTime * 2f);
+        transform.LookAt(m_currentPos);
+    }
+}
