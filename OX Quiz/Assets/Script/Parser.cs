682d3e323326c88b916c6d981949d746f6f34c20
HyunDo_Lee <godd7776@gmail.com>
2017-10-21 13:52:39 +0900

    InGame Scene, Parser Script, QuizManager Script

---
+++ OX Quiz/Assets/Script/Parser.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class Parser : MonoBehaviour
+{
+    public static List<string[]> Parse(string fileName)
+    {
+        string filePath = Application.dataPath + "/" + fileName + ".txt";
+
+        FileStream fs = new FileStream(filePath, FileMode.Open);
+        StreamReader sr = new StreamReader(fs, System.Text.Encoding.Default, true);
+
+        List<string[]> quizList = new List<string[]>();
+
+        string source = sr.ReadLine();
+
+        string[] values;
+
+        while (source != null)
+        {
+            values = source.Split('^');
+
+            if (values.Length == 0)
+            {
+                sr.Close();
+                break;
+            }
+
+            quizList.Add(values);
+
+            source = sr.ReadLine();
+        }
+
+        return quizList;
+    }
+}
---
+++ OX Quiz/Assets/Script/QuizManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text questionText;
+
+    const int question = 0;
+    const int answer = 1;
+
+    private int questionCount;
+    private List<string[]> quizList;
+
+    private void Awake()
+    {
+        quizList = Parser.Parse("QuizList");
+
+        questionCount = 0;
+
+        questionText.text = quizList[questionCount][question];
+    }
+}
