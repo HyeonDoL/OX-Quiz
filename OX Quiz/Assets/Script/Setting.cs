fec1c053e4b1cc969082bf17794c86a760ea9624
HyunDo_Lee <godd7776@gmail.com>
2017-10-21 15:45:00 +0900

    (Create) OX Voxel Model, Setting.cs, HandUp Animation

---
+++ OX Quiz/Assets/Script/Setting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Setting : MonoBehaviour
+{
+    public int maxQuestionCount { private set; get; }
+    public int maxLimitTime { private set; get; }
+
+    public bool isRandom { private set; get; }
+
+    private void Awake()
+    {
+        string[] setting = Parser.Parse("Setting")[0];
+
+        int temp;
+
+        int.TryParse(setting[0], out temp);
+        maxQuestionCount = temp;
+
+        int.TryParse(setting[1], out temp);
+        maxLimitTime = temp;
+
+        isRandom = setting[2].Equals('O');
+
+        if(isRandom)
+        {
+            // TODO : List를 랜덤하게 섞기
+        }
+    }
+}
