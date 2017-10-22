650cc2194140104a5527c5d5d7ff4edfccdea2c5
DESKTOP-B3EKVPO\YEOBI <wts1597@gmail.com>
2017-10-23 04:16:08 +0900

    (Create) Intro Animation

---
+++ OX Quiz/Assets/Script/Manager/Intro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+    [SerializeField] private GameObject m_fadePanel;
+    [SerializeField] private GameObject m_camera;
+    [SerializeField] private GameObject m_referee;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PlayIntro());
+	}
+	
+    private IEnumerator PlayIntro()
+    {
+        m_fadePanel.GetComponent<Animation>().Play();
+
+        yield return new WaitForSeconds(1f);
+    }
+}
