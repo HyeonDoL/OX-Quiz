993843703405d8b39c232d81368c02aced7e5834
HyunDo_Lee <godd7776@gmail.com>
2017-10-22 18:09:28 +0900

    Fixed AiMove
    Create AiMovementManager, DebugBox

---
+++ OX Quiz/Assets/Script/AI/AiMovementManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMovementManager : MonoBehaviour
+{
+    private static AiMovementManager instance = null;
+    public static AiMovementManager Instance
+    {
+        get
+        {
+            if (instance)
+                return instance;
+            else
+                return instance = new GameObject("AiMovementManager").AddComponent<AiMovementManager>();
+        }
+    }
+    
+    [System.Serializable]
+    public struct Rect
+    {
+        public Transform Left;
+        public Transform Right;
+        public Transform Top;
+        public Transform Bottom;
+    }
+
+    [SerializeField]
+    public Rect xSignRect;
+
+    [SerializeField]
+    public Rect oSignRect;
+
+    private List<IAiMove> aiMoveEventList;
+
+    public void AddMoveEvent(IAiMove aiMoveEvent)
+    {
+        aiMoveEventList.Add(aiMoveEvent);
+    }
+
+    //public void MoveAi()
+    //{
+    //    for (int i = 0; i < aiMoveEventList.Count; ++i)
+    //    {
+    //        aiMoveEventList[i].Move(GetRandomPosition());
+    //    }
+    //}
+    //private Vector3 GetRandomPosition()
+    //{
+
+    //}
+}
+
+public interface IAiMove
+{
+    void Move(Vector3 targetPosition);
+}
